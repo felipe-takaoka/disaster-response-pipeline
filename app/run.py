d982519ccc64ff33443b09beb0c7074dfea70045
felipe-takaoka <fehtakaoka@gmail.com>
2021-07-18 21:18:39 -0300

    feat: add code for creating 3 charts

--- app/run.py
+++ app/run.py
@@ -1,30 +1,25 @@
+import sys
 import json
 import plotly
+import joblib
 import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from sqlalchemy import create_engine
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-import joblib
-#from sklearn.externals import joblib
-from sqlalchemy import create_engine
+from plotly.graph_objs import Bar, Scatter
 
 
 app = Flask(__name__)
+sys.path.append('..')
+import re
+from models.train_classifier import tokenize
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -33,44 +28,137 @@ df = pd.read_sql_table('messages', engine)
 # load model
 model = joblib.load("../models/classifier.pkl")
 
+print('Creating charts...')
+# Chart 1: Distribution of message genres
+genre_counts = df.groupby('genre').count()['message']
+genre_names = list(genre_counts.index)
+
+# Chart 2: Top Tokens in Sampled Dataset
+def tokenizeWithoutStopWordsAndPunctuation(txt):
+    txt = re.sub(r'[^a-zA-Z0-9]', ' ', txt)
+    tokens = tokenize(txt)
+    return [t for t in tokens if t not in stopwords.words('english')]
+X, Y = df['message'], df.iloc[:,4:]
+_, X_test, _, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+# Sample dataset to reduce runtime
+tokens = X_test.sample(5000).apply(tokenizeWithoutStopWordsAndPunctuation).explode()
+top_tokens = tokens.value_counts(ascending=True, normalize=True).tail(10)
+
+# Chart 3: Top categories
+categories_count = Y_test.sum()
+top_categories = categories_count.sort_values().tail(10)
+
+# Chart 4: F1 Score x Percentage of Positive Classes
+Y_pred = model.predict(X_test)
+f1_scores = {'category': [], 'positive': [], 'f1_score': []}
+# Get F1 Score for each category
+for i, category in enumerate(Y_test.columns):
+    # Drop classification 2
+    bin = pd.concat([Y_test.iloc[:,i].reset_index(drop=True), pd.Series(Y_pred[:,i])], axis=1)
+    bin.columns = ['true', 'pred']
+    bin = bin[(bin['true']!=2) & (bin['pred']!=2)]
+    
+    # Skip categories with no true/predicted labels
+    if (any(bin.sum() == 0)):
+        continue
+
+    f1_scores['category'].append(category)
+    f1_scores['positive'].append(bin['true'].mean())
+    f1_scores['f1_score'].append(f1_score(bin['true'], bin['pred']))
+
+# create visuals
+graphs = [
+    { # Chart 1
+        'data': [
+            Bar(
+                x=genre_names,
+                y=genre_counts
+            )
+        ],
+
+        'layout': {
+            'title': 'Distribution of Message Genres',
+            'yaxis': {
+                'title': "Count"
+            },
+            'xaxis': {
+                'title': "Genre"
+            }
+        }
+    },
+
+    { # Chart 2
+        'data': [
+            Bar(
+                x=top_tokens.values,
+                y=top_tokens.index,
+                orientation='h'
+            )
+        ],
+
+        'layout': {
+            'title': 'Top Tokens in Dataset',
+            'yaxis': {
+                'title': "Token"
+            },
+            'xaxis': {
+                'title': "Frequency"
+            }
+        }
+    },
+
+    { # Chart 3
+        'data': [
+            Bar(
+                x=top_categories.values,
+                y=top_categories.index,
+                orientation='h'
+            )
+        ],
+
+        'layout': {
+            'title': 'Top Positive Categories',
+            'yaxis': {
+                'title': "Category"
+            },
+            'xaxis': {
+                'title': "Quantity of Positive Samples"
+            }
+        }
+    },
+
+    { # Chart 4
+        'data': [
+            Scatter(
+                x=f1_scores['positive'],
+                y=f1_scores['f1_score'],
+                text=f1_scores['category'],
+                mode='markers'
+            )
+        ],
+
+        'layout': {
+            'title': 'Influence of Percentage of Positive Samples on F1 Score',
+            'yaxis': {
+                'title': "F1 Score"
+            },
+            'xaxis': {
+                'title': "Percentage of Positive Samples"
+            }
+        }
+    }
+]
+
+# encode plotly graphs in JSON
+ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+print('OK')
+
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Genre"
-                }
-            }
-        }
-    ]
-    
-    # encode plotly graphs in JSON
-    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -89,7 +177,9 @@ def go():
     return render_template(
         'go.html',
         query=query,
-        classification_result=classification_results
+        classification_result=classification_results,
+        ids=ids,
+        graphJSON=graphJSON
     )
 
 
